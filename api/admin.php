e8c2b838693ba3f7492cc7b2145a10c555e62439
Savan <rs.savanpatel@gmail.com>
2025-08-14 21:37:18 +0530

    Changes

---
+++ api/admin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace api;
+include __DIR__ . '/../db/conn.php';
+
+class Admin
+{
+    private $conn;
+
+    public function __construct($dbConn)
+    {
+        $this->conn = $dbConn;
+    }
+
+    public function getAdminDetails()
+    {
+        $query = "SELECT * FROM `admin`";
+        if (mysqli_query($this->conn, $query)) {
+            return ["error" => mysqli_error($this->conn)];
+        }
+        $admins = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $admins[] = $row;
+        }
+        return $admins;
+    }
+}
